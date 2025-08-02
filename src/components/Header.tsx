0ed1a95bbacdd39be72adaf2fe61fa44191e8863
Francisco  Vieira <110839009+Francisco-tads@users.noreply.github.com>
2025-08-02 20:07:17 -0300

    Reduce logo size slightly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,7 +42,7 @@ const Header: React.FC = () => {
             <img 
               src="https://raw.githubusercontent.com/Francisco-tads/GSET-Comunica-o-Visual/refs/heads/master/src/img/logo-gset2.png" 
               alt="GSET Comunicação Visual" 
-             className="h-10 md:h-12 lg:h-16 w-auto"
+             className="h-8 md:h-10 lg:h-12 w-auto"
             />
           </a>
 
